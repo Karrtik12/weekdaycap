d2cdaa64d6e74c739d630636e08bc0cb9284a542
Karrtik12 <kartik.chat1212@gmail.com>
2024-05-07 02:52:17 +0530

    filter logic + data

--- src/App.js
+++ src/App.js
@@ -45,9 +45,132 @@ const App = () => {
         }
       });
   };
-
+  const renderData = (filters) => {
+    console.log(filters);
+    return (
+      <div style={{ margin: "50px" }}>
+        <Grid container spacing={10}>
+          {data
+            .filter((item) => {
+              if (
+                filters.selectedExperience !== "" &&
+                (filters.selectedExperience < item.minExp ||
+                  filters.selectedExperience > item.maxExp)
+              )
+                return false;
+              if (
+                filters.selectedRole.length !== 0 &&
+                filters.selectedRole.includes(item.jobRole) === false
+              )
+                return false;
+              if (
+                filters.selectedSalary !== "" &&
+                (filters.selectedSalary < item.minJdSalary ||
+                  filters.selectedSalary > item.maxJdSalary)
+              )
+                return false;
+              if (
+                filters.selectedCompanyName !== "" &&
+                item.companyName
+                  .toLowerCase()
+                  .includes(filters.selectedCompanyName.toLowerCase()) === false
+              )
+                return false;
+              return true;
+            })
+            .map((item, index) => (
+              <Card key={index} item={item} />
+            ))}
+        </Grid>
+      </div>
+    );
+  };
   return (
     <>
+      <div className="filters">
+        <FormControl sx={{ m: 1, width: "150px" }}>
+          <InputLabel>Role</InputLabel>
+          <Select
+            multiple
+            value={selectedRole}
+            onChange={(e) => setSelectedRole(e.target.value)}
+            input={<OutlinedInput label="Multiple Select" />}
+          >
+            {Roles.map((name) => (
+              <MenuItem key={name} value={name}>
+                {name.toLocaleUpperCase()}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl sx={{ m: 1, width: "150px" }}>
+          <InputLabel>Employees</InputLabel>
+          <Select
+            multiple
+            value={selectedEmployees}
+            onChange={(e) => setSelectedEmployees(e.target.value)}
+            input={<OutlinedInput label="Multiple Select" />}
+          >
+            {NumberOfEmployees.map((name) => (
+              <MenuItem key={name} value={name}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl sx={{ m: 1, width: "150px" }}>
+          <InputLabel>Experience</InputLabel>
+          <Select
+            value={selectedExperience}
+            onChange={(e) => setSelectedExperience(e.target.value)}
+            input={<OutlinedInput label="Multiple Select" />}
+          >
+            {Experience.map((name) => (
+              <MenuItem key={name} value={name}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl sx={{ m: 1, width: "150px" }}>
+          <InputLabel>Remote</InputLabel>
+          <Select
+            multiple
+            value={selectedRemote}
+            onChange={(e) => setSelectedRemote(e.target.value)}
+            input={<OutlinedInput label="Multiple Select" />}
+          >
+            {Remote.map((name) => (
+              <MenuItem key={name} value={name}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, width: "150px" }}>
+          <InputLabel>Salary</InputLabel>
+          <Select
+            value={selectedSalary}
+            onChange={(e) => setSelectedSalary(e.target.value)}
+            input={<OutlinedInput label="Multiple Select" />}
+          >
+            {MinimumBasePaySalary.map((name) => (
+              <MenuItem key={name} value={name}>
+                {name}L
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <TextField
+          id="outlined-basic"
+          label="Company Name"
+          variant="outlined"
+          onChange={(e) => setSelectedCompanyName(e.target.value)}
+        >
+          {selectedCompanyName}
+        </TextField>
+      </div>
       <InfiniteScroll
         filters={{
           selectedEmployees,
---
+++ src/Dropdown.js
@@ -0,0 +1,24 @@
+const Roles = [
+  "backend",
+  "frontend",
+  "fullstack",
+  "ios",
+  "flutter",
+  "react native",
+  "android",
+  "nlp",
+];
+const NumberOfEmployees = [
+  "1-10",
+  "11-20",
+  "21-50",
+  "51-100",
+  "101-200",
+  "201-500",
+  "500+",
+];
+const Experience = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const Remote = ["Remote", "Hybrid", "In-Office"];
+const MinimumBasePaySalary = [0, 10, 20, 30, 40, 50, 60, 70];
+
+export { Roles, NumberOfEmployees, Experience, Remote, MinimumBasePaySalary };
